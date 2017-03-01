5ccb041537c67e1d294cd44e041631a65bf5d40a
Ethan-McGregor <ethanm4@uw.edu>
2017-03-01 15:31:48 -0800

    added map activity - with mapView

---
+++ app/src/main/java/parkinglocator/jeva/washington/edu/parkinglocator/MapActivity.java
@@ -0,0 +1,14 @@
+package parkinglocator.jeva.washington.edu.parkinglocator;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class MapActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+
+}
--- app/src/main/java/parkinglocator/jeva/washington/edu/parkinglocator/StartPage.java
+++ app/src/main/java/parkinglocator/jeva/washington/edu/parkinglocator/StartPage.java
@@ -1,7 +1,9 @@
 package parkinglocator.jeva.washington.edu.parkinglocator;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class StartPage extends AppCompatActivity {
 
@@ -9,5 +11,38 @@ public class StartPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_page);
+
+        Button setLocation = (Button) findViewById(R.id.button);
+        Button find = (Button) findViewById(R.id.button2);
+        Button recent = (Button) findViewById(R.id.button3);
+
+        setLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+            }
+        });
+
+        find.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+            }
+        });
+
+        recent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+            }
+        });
+
+
     }
 }

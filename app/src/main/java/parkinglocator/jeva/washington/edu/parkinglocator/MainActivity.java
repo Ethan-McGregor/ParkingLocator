973fa1e514a62983bffe3baab2c00f982924217b
Ethan-McGregor <ethanm4@uw.edu>
2017-03-06 14:08:33 -0800

    added firebase auth

--- app/src/main/java/parkinglocator/jeva/washington/edu/parkinglocator/MainActivity.java
+++ app/src/main/java/parkinglocator/jeva/washington/edu/parkinglocator/MainActivity.java
@@ -6,14 +6,20 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
 public class MainActivity extends AppCompatActivity {
     private TabLayout tabLayout;
     private ViewPager viewPager;
+    private StorageReference mStorageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mStorageRef = FirebaseStorage.getInstance().getReference();
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);

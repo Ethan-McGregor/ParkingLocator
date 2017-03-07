0c7701d5e58d10050ec19d8539d83967d1795d36
Ethan-McGregor <ethanm4@uw.edu>
2017-03-06 17:21:01 -0800

    uploads to database

--- app/src/main/java/parkinglocator/jeva/washington/edu/parkinglocator/InfoFragment.java
+++ app/src/main/java/parkinglocator/jeva/washington/edu/parkinglocator/InfoFragment.java
@@ -6,13 +6,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class InfoFragment extends Fragment {
-
+    private CarObject car;
 
     public InfoFragment() {
         // Required empty public constructor
@@ -22,8 +27,42 @@ public class InfoFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_info, container, false);
+
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        Button submit = (Button) view.findViewById(R.id.button2);
+
+        View.OnClickListener buttonListener = new View.OnClickListener() {
+            public void onClick(View v) {
+                EditText make = (EditText) v.getRootView().findViewById(R.id.editText1);
+                EditText model = (EditText) v.getRootView().findViewById(R.id.editText2);
+                EditText year = (EditText)v.getRootView().findViewById(R.id.editText4);
+                EditText color = (EditText) v.getRootView().findViewById(R.id.editText);
+
+
+                DatabaseReference user = database.getReference("user");
+                user.setValue("User1!");
+                FirebaseDatabase userDatabase = user.getDatabase();
+
+                DatabaseReference userMake = userDatabase.getReference("make");
+                userMake.setValue( make.getText().toString());
+
+                DatabaseReference refModel = userDatabase.getReference("model");
+                refModel.setValue( model.getText().toString());
+
+                DatabaseReference refYear = userDatabase.getReference("year");
+                refYear.setValue( year.getText().toString());
+
+                DatabaseReference refColor = userDatabase.getReference("color");
+                refColor.setValue(color.getText().toString());
+                    refColor.
+            }
+        };
+
+
+        view.findViewById(R.id.button2).setOnClickListener(buttonListener);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_info, container, false);
+        return view;
     }
 
 }

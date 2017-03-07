8cee5e594d997d1034b694ee3c8b4b2560fa46f6
Vishesh Sood <omg.itsvishesh@gmail.com>
2017-03-06 18:14:44 -0800

    changes

--- app/src/main/java/parkinglocator/jeva/washington/edu/parkinglocator/MainActivity.java
+++ app/src/main/java/parkinglocator/jeva/washington/edu/parkinglocator/MainActivity.java
@@ -25,7 +25,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mStorageRef = FirebaseStorage.getInstance().getReference();
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
